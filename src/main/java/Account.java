574427c65712d964dd875ac0ca67797c370567af
ArturMukhutdinov <muhutdinov.artur@yandex.ru>
2022-03-12 00:22:14 +0300

    commit1

--- src/main/java/Account.java
+++ src/main/java/Account.java
@@ -6,11 +6,15 @@ public class Account {
         this.name = name;
     }
 
-    public boolean checkNameToEmboss() {
+    public boolean checkNameToEmboss(Account account) {
+        return account.name != null && account.name.length() >= 3 && account.name.length() <= 19
+                && !account.name.startsWith(" ") && !account.name.endsWith(" ")
+                && !account.name.contains("" + " " + " " + "") && account.name.contains(" ");
         /*
              Этот метод должен проверять, что сохранённая через конструктор строка соответствует требованиям.
              Если строка удовлетворяет условиям, метод возвращает true, иначе — false.
          */
-    }
 
+
+    }
 }
